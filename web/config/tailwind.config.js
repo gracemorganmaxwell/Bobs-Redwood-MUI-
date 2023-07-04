9426d4c8ed42ecd8a62128c8fd1a12427fddd1c0
grace morgan-maxwell <gracemorganmaxwell@gmail.com>
2023-07-04 20:32:07 +1200

    commit: updates to tailwind setup

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const withMT = require('@material-tailwind/react/utils/withMT')
 module.exports = {
-  content: ['src/**/*.{js,jsx,ts,tsx}'],
+  content: ['src/**/*.{js,jsx,ts,tsx}',
+  'websiteLandscaping/my-redwood-project/node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
+    'websiteLandscaping/my-redwood-project/node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
+],
   theme: {
-    extend: {},
+    extend: {
+      colors: { almostBlack: '#242424',
+        lemonLime: '#E6FF00', /* safety green */
+        onyx: '##111111', /* almost black */
+        nero: '#252525' /* dark gray */,
+        dolphin: '#7e7f9a' /* purple gray */,
+        turquoise: '#40ebd4' /* turquoise */,
+        oriolesOrange: '#fe4e1c' /* red orange */,
+        sunsetOrange: '#ff6054' /* coral */,
+        chartreuse: '#80ff01' /* green */,
+        tamarillo: '#911412' /* deep red */,
+        eerieBlack: '#1e1e1e' /* almost black */,
+        jet: '#353535' /* lighter - almost black rollover */,
+    },
+    },
+    fontFamily: {
+      modern: ['Abril Fatface', cursive],
+      sanSerif: ['Inter', sans-serif],
+    },
   },
   plugins: [],
 }
